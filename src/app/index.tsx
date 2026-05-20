76eb3d639f744dec98a07dc9b6254f3b4d660662
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-05-20 19:36:14 -0400

    Styles in react native

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
 import * as Device from "expo-device";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Macrozone!</Text>
-      <Text>Running on: {Platform.OS}</Text>
+      <Text style={styles.title}>Welcome to Macrozone!</Text>
+      <Text style={styles.date}>Monday, March 16</Text>
       <Text>Device Model: {Device.modelName}</Text>
       <Text>Device Brand: {Device.brand}</Text>
       <Text>OS version: {Device.osVersion} </Text>
@@ -16,7 +16,21 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#1a1a2e",
+    paddingTop: 60,
+    paddingHorizontal: 20,
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#ffffffff",
+  },
+  date: {
+    fontSize: 14,
+    color: "a0a0b0",
+    marginTop: 4,
+    marginBottom: 30,
   },
 });
