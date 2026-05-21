37fdaace1fa37ec317b6ffb4a5a04a32440a9035
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-05-20 20:29:55 -0400

    Home header and header texst

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,14 @@
+import HomeHeader from "@/components/HomeHeader";
 import { globalStyles } from "@/styles/global";
-import * as Device from "expo-device";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.title}>Welcome to Macrozone!</Text>
-      <Text style={styles.date}>Monday, March 16</Text>
-      <Text>Device Model: {Device.modelName}</Text>
-      <Text>Device Brand: {Device.brand}</Text>
-      <Text>OS version: {Device.osVersion} </Text>
+      {/* <Text style={styles.date}>Monday, March 16</Text> */}
+
+      <HomeHeader />
     </View>
   );
 }
