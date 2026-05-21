75103dde540ce4d081ce2e7305017da8bd9c3ad4
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-05-20 20:07:10 -0400

    Using global css

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
+import { globalStyles } from "@/styles/global";
 import * as Device from "expo-device";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Macrozone!</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>Welcome to Macrozone!</Text>
       <Text style={styles.date}>Monday, March 16</Text>
       <Text>Device Model: {Device.modelName}</Text>
       <Text>Device Brand: {Device.brand}</Text>
@@ -14,22 +15,9 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#1a1a2e",
-    paddingTop: 60,
-    paddingHorizontal: 20,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#ffffffff",
-  },
   date: {
     fontSize: 14,
-    color: "a0a0b0",
+    color: "#a0a0b0",
     marginTop: 4,
     marginBottom: 30,
   },
