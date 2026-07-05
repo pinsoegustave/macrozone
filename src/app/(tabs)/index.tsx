dfc14f3fb41869650a1e3e92eaa3a6849c3b9dd4
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-07-05 11:41:44 -0400

    checkpoint 30: macrocards

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,14 @@
+import MacroGrid from "@/components/MacroGrid";
 import { globalStyles } from "@/styles/global";
-import { ScrollView, Text, TouchableOpacity } from "react-native";
+import { ScrollView, Text } from "react-native";
 import HomeHeader from "../../components/HomeHeader";
 
 export default function HomeScreen() {
   return (
     <ScrollView style={globalStyles.container}>
-      <Text style={globalStyles.title}>Macrozone! </Text>
+      <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
-      <TouchableOpacity style={{ marginTop: 16 }} onPress={() => {}}>
-        <Text style={{ fontSize: 18, color: "#007bff" }}>Go to Meals</Text>
-      </TouchableOpacity>
+      <MacroGrid />
     </ScrollView>
   );
 }
