307ef17b45c6749aa7505cc2c4e6100c53c5165c
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-07-05 13:01:31 -0400

    checkpoint 42: display meals

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,33 @@
 import MacroGrid from "@/components/MacroGrid";
 import RecentMeals from "@/components/RecentMeals";
+import { getMeals, Meal } from "@/storage/meals";
 import { globalStyles } from "@/styles/global";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
 import { ScrollView, Text } from "react-native";
 import HomeHeader from "../../components/HomeHeader";
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log("Loaded Meals:", data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
-      <MacroGrid />
-      <RecentMeals />
+      <MacroGrid meals={meals} />
+      <RecentMeals meals={meals} />
     </ScrollView>
   );
 }
